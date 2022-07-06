be470a51a7802ce8948b38c9b5ff70738f887ef3
unknown <sntakirutimana72@gmail.com>
2022-07-06 21:33:23 +0200

    Remove unnecessary code lines

--- src/helpers/listManager.js
+++ src/helpers/listManager.js
@@ -1,15 +1,12 @@
 const DATA_KEY = 'ToDoList';
 
-const data = { [DATA_KEY]: [] };
-
-export const getItems = () => data[DATA_KEY]; // {
-//   const items = localStorage.getItem(DATA_KEY);
-//   return items ? JSON.parse(items) : [];
-// };
+export const getItems = () => {
+  const items = localStorage.getItem(DATA_KEY);
+  return items ? JSON.parse(items) : [];
+};
 
 export const storeItems = (items) => {
-  // localStorage.setItem(DATA_KEY, JSON.stringify(items));
-  data[DATA_KEY] = items;
+  localStorage.setItem(DATA_KEY, JSON.stringify(items));
 };
 
 export const hasFulfilled = (items) => items.find(
